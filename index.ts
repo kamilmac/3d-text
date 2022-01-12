667a9095d4dd4f085da778ffc8bffd222bec608e
kamilmac <kmacinski@gmail.com>
2022-01-12 20:53:09 +0100

    fixed

--- index.ts
+++ index.ts
@@ -1,86 +1,63 @@
-import './index.css';
+import './index.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
-import { TextGeometry } from 'three/examples/jsm/geometries/TextGeometry.js'
-
-// import typefaceFont from 'three/examples/fonts/helvetiker_regular.typeface.json'
-
-import { FontLoader } from 'three/examples/jsm/loaders/FontLoader.js'
-
-const fontLoader = new FontLoader()
-
-const textureLoader = new THREE.TextureLoader()
-const matcapTexture = textureLoader.load('/textures/checkerboard-8x8.png')
-
-const scene = new THREE.Scene()
-
-const textMaterial = new THREE.MeshMatcapMaterial({ matcap: matcapTexture })
-fontLoader.load('/helvetiker_regular.typeface.json', (font) => {
-    console.log('font loaded')
-    const textGeometry = new TextGeometry(
-        'Floating do nots',
-        {
-            font,
-            size: 0.5,
-            height: 0.2,
-            curveSegments: 12,
-            bevelEnabled: true,
-            bevelThickness: 0.03,
-            bevelSize: 0.02,
-            bevelOffset: 0,
-            bevelSegments: 5,
-        }
-    )
-   
-    // textMaterial.wireframe = true
-    const text = new THREE.Mesh(textGeometry, textMaterial)
-    scene.add(text)
-    textGeometry.computeBoundingBox()
-    textGeometry.translate(
-        - (textGeometry.boundingBox.max.x - 0.02) * 0.5, // Subtract bevel size
-        - (textGeometry.boundingBox.max.y - 0.02) * 0.5, // Subtract bevel size
-        - (textGeometry.boundingBox.max.z - 0.03) * 0.5  // Subtract bevel thickness
-    )
-    // textGeometry.center()
-})
-
-const donutGeometry = new THREE.TorusGeometry(0.3, 0.2, 20, 45)
-const donutMaterial = new THREE.MeshMatcapMaterial({ matcap: matcapTexture })
-
-for(let i = 0; i < 100; i++)
-{
-    const donut = new THREE.Mesh(donutGeometry, textMaterial)
-    donut.position.x = (Math.random() - 0.5) * 10
-    donut.position.y = (Math.random() - 0.5) * 10
-    donut.position.z = (Math.random() - 0.5) * 10
-    donut.rotation.x = Math.random() * Math.PI
-    donut.rotation.y = Math.random() * Math.PI
-    const scale = Math.random()
-    donut.scale.set(scale, scale, scale)
-    scene.add(donut)
-}
-
 
 /**
  * Base
  */
+// Debug
+
 // Canvas
 const canvas = document.querySelector('#canvas')
 
 // Scene
+const scene = new THREE.Scene()
 
 /**
- * Object
+ * Lights
  */
-const geometry = new THREE.BoxGeometry(1, 1, 1, 3, 3, 3)
-// const material = new THREE.MeshBasicMaterial({
-//     map: colorTexture
-// })
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.5)
+scene.add(ambientLight)
 
-const material = new THREE.MeshBasicMaterial()
+const pointLight = new THREE.PointLight(0xffffff, 0.5)
+pointLight.position.x = 2
+pointLight.position.y = 3
+pointLight.position.z = 4
+scene.add(pointLight)
 
-const mesh = new THREE.Mesh(geometry, material)
-// scene.add(mesh)
+/**
+ * Objects
+ */
+// Material
+const material = new THREE.MeshStandardMaterial()
+material.roughness = 0.4
+
+// Objects
+const sphere = new THREE.Mesh(
+    new THREE.SphereGeometry(0.5, 32, 32),
+    material
+)
+sphere.position.x = - 1.5
+
+const cube = new THREE.Mesh(
+    new THREE.BoxGeometry(0.75, 0.75, 0.75),
+    material
+)
+
+const torus = new THREE.Mesh(
+    new THREE.TorusGeometry(0.3, 0.2, 32, 64),
+    material
+)
+torus.position.x = 1.5
+
+const plane = new THREE.Mesh(
+    new THREE.PlaneGeometry(5, 5),
+    material
+)
+plane.rotation.x = - Math.PI * 0.5
+plane.position.y = - 0.65
+
+scene.add(sphere, cube, torus, plane)
 
 /**
  * Sizes
@@ -90,21 +67,19 @@ const sizes = {
     height: window.innerHeight
 }
 
-window.addEventListener('resize', () => {
-  sizes.width = window.innerWidth
-  sizes.height = window.innerHeight
-  camera.aspect = window.innerWidth / window.innerHeight
-  camera.updateProjectionMatrix()
-  renderer.setSize(sizes.width, sizes.height)
-  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-})
+window.addEventListener('resize', () =>
+{
+    // Update sizes
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
 
-window.addEventListener('dblclick', () => {
-    if (!document.fullscreenElement) {
-        canvas.requestFullscreen()
-    } else {
-        document.exitFullscreen()
-    }
+    // Update camera
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 })
 
 /**
@@ -112,7 +87,9 @@ window.addEventListener('dblclick', () => {
  */
 // Base camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-camera.position.z = 3
+camera.position.x = 1
+camera.position.y = 1
+camera.position.z = 2
 scene.add(camera)
 
 // Controls
@@ -123,11 +100,11 @@ controls.enableDamping = true
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas,
-    antialias: true
+    canvas: canvas
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
 /**
  * Animate
  */
@@ -136,6 +113,15 @@ const clock = new THREE.Clock()
 const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
+
+    // Update objects
+    sphere.rotation.y = 0.1 * elapsedTime
+    cube.rotation.y = 0.1 * elapsedTime
+    torus.rotation.y = 0.1 * elapsedTime
+
+    sphere.rotation.x = 0.15 * elapsedTime
+    cube.rotation.x = 0.15 * elapsedTime
+    torus.rotation.x = 0.15 * elapsedTime
 
     // Update controls
     controls.update()
